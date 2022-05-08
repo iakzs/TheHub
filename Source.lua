4cf62424bc5cc81d86b5cc10652b6a0fe25602c7
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-08 19:38:40 -0400

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 6872265039 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/iakzs/TheHub/main/scripts/BedWars.lua?token=GHSAT0AAAAAABULGABP7SCSTBJZJMI77BQKYTYKI7A"))()
+  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/BedWars.lua", true))()
 elseif game.PlaceId == 855499080 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/iakzs/TheHub/main/scripts/SkyWars.lua?token=GHSAT0AAAAAABULGABPXZK3T75XV7J5HXM2YTYKJPQ"))()
+  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/SkyWars.lua", true))()
 elseif game.PlaceId == 2248408710 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/iakzs/TheHub/main/scripts/DestructionSimulator.lua?token=GHSAT0AAAAAABULGABPEQLXRHO6WNS6AVVYYTYKJRA"))()
+  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/DestructionSimulator.lua", true))()
 end
