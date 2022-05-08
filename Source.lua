d381418543ca2124d03b0ac2687c57292ee95441
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-08 17:49:55 -0400

    Add files via upload

---
+++ Source.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 4581966615 then
+  loadstring(game:HttpGet("https://raw.github.com/iakzs/TheHub/blob/main/scripts/Anomic.lua"))()
+elseif game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.github.com/iakzs/TheHub/blob/main/scripts/Arsenal.lua"))()
+elseif game.PlaceId == 606849621 then
+  loadstring(game:HttpGet("https://raw.github.com/iakzs/TheHub/blob/main/scripts/Jailbreak.lua"))()
+end
