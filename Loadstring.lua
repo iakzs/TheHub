1f4286ae3aade2605cba9b884d8052d2275e7328
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-08 19:35:28 -0400

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iakzs/TheHub/main/Source.lua?token=GHSAT0AAAAAABULGABOIN5CECTOBVTKGYTMYTYKMKQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iakzs/TheHub/main/Source.lua?token=GHSAT0AAAAAABULGABOIN5CECTOBVTKGYTMYTYKMKQ", true))()
