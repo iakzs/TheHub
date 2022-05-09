cbd98eb9cb1b4a49cd5b1c8ce04ec6762d45d68a
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-08 20:06:00 -0400

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,5 +1,5 @@
-if game.PlaceId == 6872265039 then
-  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/BedWars.lua", true))()
+if game.PlaceId == 0 then
+  loadstring(game:HttpGet("", true))()
 elseif game.PlaceId == 855499080 then
   loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/SkyWars.lua", true))()
 elseif game.PlaceId == 2248408710 then
