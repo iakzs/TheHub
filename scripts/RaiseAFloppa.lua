9f7a578dd0a487069667e3fb31d6188a2f920cee
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-11 17:47:39 -0400

    Add files via upload

---
+++ scripts/RaiseAFloppa.lua
@@ -0,0 +1,5 @@
+getgenv().autoshitcolmoney = true -- collects floppa's money instantly
+getgenv().autosave = true -- auto saves every 10 seconds
+getgenv().autocleanshit = true -- cleans litter box automatically
+getgenv().autobuyfood = true -- auto buys food and places it when floppa is hungry
+
