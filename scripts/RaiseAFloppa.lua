aaf712a8d08e64a5a250b21b0c7e335557f60a1d
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-11 17:50:15 -0400

    Update RaiseAFloppa.lua
    loadstring lol

--- scripts/RaiseAFloppa.lua
+++ scripts/RaiseAFloppa.lua
@@ -3,3 +3,4 @@ getgenv().autosave = true -- auto saves every 10 seconds
 getgenv().autocleanshit = true -- cleans litter box automatically
 getgenv().autobuyfood = true -- auto buys food and places it when floppa is hungry
 
+loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/RaiseAFloppa2.lua", true))()
