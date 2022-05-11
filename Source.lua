f62714c22929500bd1c1205863958fb701244fee
iakzs <105176207+iakzs@users.noreply.github.com>
2022-05-11 17:52:16 -0400

    Update Source.lua
    Raise a floppa script

--- Source.lua
+++ Source.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 855499080 then
   loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/SkyWars.lua", true))()
 elseif game.PlaceId == 2248408710 then
   loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/DestructionSimulator.lua", true))()
+elseif game.PlaceId == 9203864304 then
+  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/RaiseAFloppa.lua", true))()
 end
