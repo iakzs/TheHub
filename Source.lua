f2beccd7de09f26e06c0020b6eef8b138a934cc2
iakzs <105176207+iakzs@users.noreply.github.com>
2023-01-25 18:36:28 -0300

    Update Source.lua
    added SWJPD and if not found script, print a message

--- Source.lua
+++ Source.lua
@@ -10,4 +10,7 @@ elseif game.PlaceId == 70005410 then
   loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/BloxHunt.lua", true))()
 elseif game.PlaceId == 205224386 then
   loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/HideNSeekExtreme.lua", true))()
+elseif game.PlaceId == 7933242294 then
+  loadstring(game:HttpGet("https://github.com/iakzs/TheHub/raw/main/scripts/StudWallsJumpPerDifficulty.lua", true))()
+elseif print("No scripts for this place! try with other")
   end
